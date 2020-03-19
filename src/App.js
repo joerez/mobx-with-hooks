865556ae89663a670db29feac94eff7fb1608fb4
JoeRez <rezenbiz@gmail.com>
2020-03-18 21:56:41 -0700

    MobX with hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// https://www.youtube.com/watch?v=pnhIJA64ByY
 
-function App() {
+import React from 'react';
+import { useLocalStore, useObserver } from 'mobx-react';
+
+const StoreContext = React.createContext();
+
+const StoreProvider = ({children}) => {
+  const store = useLocalStore(() => ({
+    bugs: ['Centipede'],
+    addBug: (bug) => {
+      store.bugs.push(bug);
+    },
+    get bugsCount() {
+      return store.bugs.length;
+    }
+  }))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <StoreContext.Provider value={store}>
+      {children}
+    </StoreContext.Provider>
+  )
 }
 
-export default App;
+const BugsHeader = () => {
+  const store = React.useContext(StoreContext);
+
+  return useObserver(() => (
+    <h1>{store.bugsCount} Bugs!</h1>
+  ))
+}
+
+const BugsList = () => {
+  const store = React.useContext(StoreContext);
+
+  return useObserver(() => (
+    <ul>
+      {store.bugs.map((bug) => <li key={bug}>{bug}</li>)}
+    </ul>
+  ))
+}
+
+const BugsForm = () => {
+  const store = React.useContext(StoreContext);
+
+  const [bug, setBug] = React.useState('');
+
+  return (
+    <form onSubmit={e => {
+      store.addBug(bug);
+      setBug('');
+      e.preventDefault();
+    }}>
+      <input type="text" value={bug} onChange={e => setBug(e.target.value)} />
+      <button type="submit">Add</button>
+    </form>
+  )
+}
+
+export default function App() {
+  return (
+      <StoreProvider>
+        <BugsHeader />
+        <main><BugsList /></main>
+        <BugsForm />
+      </StoreProvider>
+    )
+}
